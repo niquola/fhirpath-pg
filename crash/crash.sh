988a27f51f2e829aa1032b8b6ce38688b6be863f
Aitem <muradbei@gmail.com>
2017-02-07 16:01:49 +0300

    crash

--- crash/crash.sh
+++ crash/crash.sh
@@ -1,56 +1,19 @@
 #!/bin/bash
 set -e
 
-RESOURCE='{
-	"number":{"value": 42,
-						"array": [40, 42, 43]},
+RESOURCE=$(<resource.json)
 
-	"date": {"value": "1980-02-05T08:30" ,
-					 "array": ["1980-02-05T08:30", "1976-01", "1952-02-03"]},
+# All search types from doc https://www.hl7.org/fhir/search.html
+# SEARCH_TYPES=(date number reference string token uri quantity)
 
-	"string": {"value": "Test",
-						 "array": ["Test", "Crush", "Fhirpath"]},
-
-	"token": {"value": {"use": "usual",
-											"type": { "coding": [
-												{"system": "sys1", "code": "code1"},
-												{"system": "sys2", "code": "code2"}
-											]},
-											"system": "isys",
-											"value": "value1",
-											"period": {"start": "2001-05-06"},
-											"assigner": {"display": "Acme Healthcare"} },
-					 "array": [ {"use": "usual",
-										 	 "type": { "coding": [
-													{"system": "sys1", "code": "code1"},
-													{"system": "sys2", "code": "code2"} ]},
-											 "system": "isys",
-											 "value": "value1",
-											 "period": {"start": "2001-05-06"},
-											 "assigner": {"display": "Acme Healthcare"}
-											}, {
-											 "use": "official",
-											 "type": { "coding": [ {
-													"system": "itsys", "code": "itcode"}
-									 			]},
-											 "system": "isys2",
-											 "value": "ival2" }]},
-
-	"reference": {"value": {"reference": "http://dom.io/ups/Patient/1"},
-								"array": [{"reference": "http://dom.io/ups/Patient/1"},
-													{"reference": "http://dom.io/ups/Patient/1"}]},
-	"composite": {},
-	"quantity": {},
-	"uri": {},
-	"_empty_obj": {},
-	"_empty_arr": [],
-	"_empty_str": ""
-}'
-
-#SEARCH_TYPES=(date number reference string token uri quantity)
+# Supported search types
 SEARCH_TYPES=(date number reference string token)
+
+# Data types https://www.hl7.org/FHIR/datatypes.html
 PRIMITIVE_DATA_TYPES=(integer decimal unsignedInt positiveInt instant time date dateTime boolean string code id markdown uri oid base64Binary)
+
 COMPLEX_DATA_TYPES=(Ratio SampledData Period Quantity Age Distance SimpleQuantity Duration Count Money Attachment Range Coding CodeableConcept HumanName Address ContactPoint Identifier Timing Signature Annotation)
+
 DATA_TYPES=("${PRIMITIVE_DATA_TYPES[@]}" "${COMPLEX_DATA_TYPES[@]}")
 
 
@@ -75,11 +38,6 @@ function mk_query() {
 	fi
 
   echo "SELECT fhirpath_as_$SEARCH_TYPE('$RESOURCE', '$PTH', '$DATA_TYPE' $MODIFICATION);"
-}
-
-function error_generator () {
-	local PATH_PREFIX=$1
-	echo $PATH_PREFIX
 }
 
 for st in "${SEARCH_TYPES[@]}"
