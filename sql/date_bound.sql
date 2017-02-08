1a32eb2f0832716bd1d5757f2384d68b4d943f9d
niquola <niquola@gmail.com>
2017-02-08 16:10:56 +0300

    more tests for date_bound

--- sql/date_bound.sql
+++ sql/date_bound.sql
@@ -9,3 +9,7 @@ SELECT fhirpath_date_bound('1980-02', 'max');
 SELECT fhirpath_date_bound('1980-02-05','max'); 
 SELECT fhirpath_date_bound('1980-02-05T08', 'max'); 
 SELECT fhirpath_date_bound('1980-02-05T08:30', 'max'); 
+
+
+select fhirpath_date_bound('2000-01-01', 'min');
+select fhirpath_date_bound('2000-01-01', 'max');
