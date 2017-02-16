fef68ac138dcd7fdf438ae48c88f82ee791db76b
Aitem <muradbei@gmail.com>
2017-02-16 18:09:11 +0300

    performance test

--- performance/test.sql
+++ performance/test.sql
@@ -1,189 +1,14 @@
 --db:-h 127.0.0.1 -U root
 
-
----{{{
-
-\c postgres
-\timing
-
-select count(num) "fhirpath number .e.d.c.a.b" from (
-	select fhirpath_as_number(('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
-		from generate_series(1, 100000) x
-) _ ;
-
-select count(num) "native number {e,d,c,a,b}" from (
-	select ((('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb)#>>'{e,d,c,a,b}')::numeric as num
-		from generate_series(1, 100000) x
-) _;
-
-select count(num) "fhirpath number .b" from (
-	select fhirpath_as_number(('{"b": ' || x || '}')::jsonb, '.b','integer', 'max') as num
-		from generate_series(1, 100000) x
-) _ ;
-
-select count(num) "native number {b}" from (
-	select ((('{"b": ' || x || '}')::jsonb)#>>'{b}')::numeric as num
-		from generate_series(1, 100000) x
-) _;
-
-select count(num) "fhirpath as_date .b" from (
-	select fhirpath_as_date(('{"b": "2017-02-15T16:34:00.' || x || '"}')::jsonb, '.b', 'date', 'max') as num
-		from generate_series(1, 100000) x
-) _ ;
-
-select count(num) "native date native {b}" from (
-	select ((('{"b": "2017-02-15T16:34:00.' || x || '"}')::jsonb)#>>'{b}')::date as num
-		from generate_series(1, 100000) x
-) _;
-
-select count(num) "fhirpath as_date .e.d.c.a.b" from (
-	select fhirpath_as_date(('{"e": {"d":{"c":{"a": {"b": "2017-02-15T16:34:00.' || x || '"} } } } }')::jsonb, '.e.d.c.a.b', 'date', 'max') as num
-		from generate_series(1, 100000) x
-) _ ;
-
-select count(num) "native date {e,d,c,a,b}" from (
-	select ((('{"e": {"d":{"c":{"a": {"b": "2017-02-15T16:34:00.' || x || '"} } } } }')::jsonb)#>>'{e,d,c,a,b}')::date as num
-		from generate_series(1, 100000) x
-) _;
-
-
-
----}}}
-
-
-
----{{{
-\c postgres
-\timing
-
-DROP function arr_max(json);
-CREATE OR REPLACE FUNCTION arr_max(json) RETURNS numeric AS
-	'select max(i::numeric) from (select json_array_elements_text($1) as i) t'
-LANGUAGE sql IMMUTABLE;
-
---select count(num) "fhirpath number .e.d.c.a.b" from (
-select count(*) from (
-	select fhirpath_as_number(('{"e":{"d":{"c":{"a":{"b":['||x||','||x+1||','||x+2||','||x-8||']} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
-		from generate_series(1, 1000000) x
-) _ ;
-
----select count(num) "native number {e,d,c,a,b}" from (
-select count(*) from (
-	 select  arr_max(((('{"e": {"d":{"c":{"a": {"b":['||x||','||x+1||','||x+2||','||x-3||']}} }} }')::jsonb)#>'{e,d,c,a,b}')::json) as num
-
-	 -- select json_array_elements(
-			-- (('{"e": {"d":{"c":{"a":{"b":['||x||','||x+1||','||x+2||']}} }} }')::jsonb)#>'{e,d,c,a,b}'
-	 -- ) as num
-	 from generate_series(1, 1000000) x
-) _;
-
-
----}}}
-
----{{{
-
-\c postgres
-\timing
-
-
-
-select count(num) "fhirpath number .e.d.c.a.b" from (
-	select fhirpath_as_number(('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
-		from generate_series(1, 100000) x
-) _ ;
-
-select count(num) "native number {e,d,c,a,b}" from (
-	select ((('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb)#>>'{e,d,c,a,b}')::numeric as num
-		from generate_series(1, 100000) x
-) _;
-
----}}}
-
 ---{{{
 \c postgres
 
-\set series 1000000
+DROP extension if exists fhirpath;
+create extension if not exists fhirpath;
 
-
-DROP function eval_time(text);
-create or replace function eval_time(fn_name text) returns interval as
-$proc$
-DECLARE
-	result integer;
-  StartTime timestamptz;
-  EndTime timestamptz;
-  Delta interval;
-BEGIN
-	StartTime := clock_timestamp();
-	execute fn_name||'()';
-	EndTime := clock_timestamp();
-	Delta := 1000 *( extract(epoch from EndTime) - extract(epoch from StartTime) );
-	return Delta;
-END;
-$proc$
-LANGUAGE plpgsql;
-
-
-
-
-create or replace function bench_fhirpath_as_number() returns numeric as $$
-BEGIN
-	perform count(num) "fhirpath number .e.d.c.a.b" from (
-		select fhirpath_as_number(('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
-		from generate_series(1, 100000) x
-	) _ ;
-END;
-$$ LANGUAGE plpgsql;
-
-select bench_fhirpath_as_number();
-select eval_time("bench_fhirpath_as_number");
-
-
-DO $proc$
-DECLARE
-  StartTime timestamptz;
-  EndTime timestamptz;
-  Delta interval;
-BEGIN
-  StartTime := clock_timestamp();
-
-	perform count(num) "fhirpath number .e.d.c.a.b" from (
-	select fhirpath_as_number(('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
-		from generate_series(1, 100000) x
-) _ ;
-
-  EndTime := clock_timestamp();
-  Delta := ( extract(epoch from EndTime) - extract(epoch from StartTime) );
-  RAISE NOTICE 'Duration in millisecs=%', Delta;
-END;
-$proc$;
-
-DO $proc$
-DECLARE
-  StartTime timestamptz;
-  EndTime timestamptz;
-  Delta interval;
-BEGIN
-  StartTime := clock_timestamp();
-
-	perform count(num) "native number {e,d,c,a,b}" from (
-	select ((('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb)#>>'{e,d,c,a,b}')::numeric as num
-		from generate_series(1, 100000) x
-) _;
-
-  EndTime := clock_timestamp();
-  Delta := ( extract(epoch from EndTime) - extract(epoch from StartTime) );
-  RAISE NOTICE 'Duration in millisecs=%', Delta;
-END;
-$proc$;
-
-
-
----}}}
-
----{{{
-\c postgres
-
+------------------------
+-- Query execution time
+------------------------
 
 DROP function eval_time(text);
 create or replace function eval_time(query text) returns numeric as
@@ -203,16 +28,130 @@ $proc$
 LANGUAGE plpgsql;
 
 
-drop function bench_fhirpath_as_number();
-create or replace function bench_fhirpath_as_number() returns void as $$
+------------------------
+-- _as_nubmer
+------------------------
+create or replace function fhirpath_as_number_1() returns void as $$
 BEGIN
-	PERFORM count(num) "fhirpath number .e.d.c.a.b" from (
-		select fhirpath_as_number(('{"e": {"d":{"c":{"a": {"b": ' || x || '} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
-		from generate_series(1, 100000) x
-	) _ ;
+	PERFORM fhirpath_as_number(('{"e":{"d":{"c":{"a": {"b":'||x||'}} }} }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
+	from generate_series(1, 100000) x;
 END;
 $$ LANGUAGE plpgsql;
 
+create or replace function native_as_number_1() returns void as $$
+BEGIN
+	PERFORM ((('{"e": {"d":{"c":{"a":{"b":'||x||'}} }} }')::jsonb)#>>'{e,d,c,a,b}')::numeric as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
 
-select eval_time('select bench_fhirpath_as_number()');
+create or replace function fhirpath_as_number_2() returns void as $$
+BEGIN
+	PERFORM fhirpath_as_number(('{"b": ' || x || '}')::jsonb, '.b','integer', 'max') as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+create or replace function native_as_number_2() returns void as $$
+BEGIN
+	PERFORM ((('{"b": ' || x || '}')::jsonb)#>>'{b}')::numeric as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+------------------------
+-- _as_date
+------------------------
+create or replace function fhirpath_as_date_1() returns void as $$
+BEGIN
+	PERFORM fhirpath_as_date(('{"b": "2017-02-15T16:34:00.' || x || '"}')::jsonb, '.b', 'date', 'max') as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+create or replace function native_as_date_1() returns void as $$
+BEGIN
+	PERFORM ((('{"b": "2017-02-15T16:34:00.' || x || '"}')::jsonb)#>>'{b}')::date as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+create or replace function fhirpath_as_date_2() returns void as $$
+BEGIN
+	PERFORM fhirpath_as_date(('{"e": {"d":{"c":{"a": {"b": "2017-02-15T16:34:00.' || x || '"} } } } }')::jsonb, '.e.d.c.a.b', 'date', 'max') as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+create or replace function native_as_date_2() returns void as $$
+BEGIN
+	PERFORM ((('{"e": {"d":{"c":{"a": {"b": "2017-02-15T16:34:00.' || x || '"} } } } }')::jsonb)#>>'{e,d,c,a,b}')::date as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+-------------------------
+--  AS NUMBER MAX
+-------------------------
+
+DROP function number_arr_max(json);
+CREATE OR REPLACE FUNCTION number_arr_max(json) RETURNS numeric AS
+	'select max(i::numeric) from (select json_array_elements_text($1) as i) t'
+LANGUAGE sql IMMUTABLE;
+
+create or replace function fhirpath_as_number_max() returns void as $$
+BEGIN
+	PERFORM fhirpath_as_number(('{"e":{"d":{"c":{"a":{"b":['||x||','||x+1||','||x+2||','||x-8||']} } } } }')::jsonb, '.e.d.c.a.b','integer', 'max') as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+create or replace function native_as_number_max() returns void as $$
+BEGIN
+	PERFORM  number_arr_max(((('{"e": {"d":{"c":{"a": {"b":['||x||','||x+1||','||x+2||','||x-4||']}} }} }')::jsonb)#>'{e,d,c,a,b}')::json) as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+-------------------------
+--  AS NUMBER MIN
+-------------------------
+create or replace function fhirpath_as_number_min() returns void as $$
+BEGIN
+	perform fhirpath_as_number(('{"e":{"d":{"c":{"a":{"b":['||x||','||x+1||','||x+2||','||x-8||']} } } } }')::jsonb, '.e.d.c.a.b','integer', 'min') as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP function number_arr_min(json);
+CREATE OR REPLACE FUNCTION number_arr_min(json) RETURNS numeric AS
+	'select min(i::numeric) from (select json_array_elements_text($1) as i) t'
+LANGUAGE sql IMMUTABLE;
+
+create or replace function native_as_number_min() returns void as $$
+BEGIN
+	perform  number_arr_min(((('{"e": {"d":{"c":{"a": {"b":['||x||','||x+1||','||x+2||','||x-6||']}} }} }')::jsonb)#>'{e,d,c,a,b}')::json) as num
+	from generate_series(1, 100000) x;
+END;
+$$ LANGUAGE plpgsql;
+
+select
+	  r.method
+	, r.fhirpath
+	, r.native
+	, r.fhirpath/r.native as "ratio fhirpath/native"
+
+from (
+	select
+			f method
+		,	eval_time('select fhirpath_'||f.f||'()') fhirpath
+		, eval_time('select native_'||f.f||'()') native
+	from (
+		select unnest(array['as_number_1', 'as_number_2', 'as_date_1', 'as_date_2','as_number_min', 'as_number_max']) as f
+	) f
+) as r
+
+order by "ratio fhirpath/native" desc;
+
+
 ---}}}
