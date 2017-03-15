be495b991dac247a43c9dee9c1b9144c6e7b12d1
Aitem <muradbei@gmail.com>
2017-03-15 17:03:17 +0300

    fix broken ploy exists

--- fhirpath.c
+++ fhirpath.c
@@ -1371,6 +1371,10 @@ fhirpath_sort_as_text(PG_FUNCTION_ARGS) {
 
 void reduce_as_exists(void *acc, JsonbValue *val){
 	BoolAccumulator *bacc = (BoolAccumulator *) acc;
+
+	TextAccumulator *tacc = (TextAccumulator *) acc;
+    char *element_type = tacc->element_type;
+  if (strcmp(element_type, "Polymorphic") == 0) return;
 	bacc->acc = true;
 }
 
